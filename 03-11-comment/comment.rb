999bcbbeffae38c5c56cadad8fbb93c41dac9a4b
nakamura <9111754+ynakayu@users.noreply.github.com>
2025-08-01 08:03:16 +0900

    追加 03-05-first-ruby 〜 03-19-string-1

---
+++ 03-11-comment/comment.rb
@@ -0,0 +1,13 @@
+# puts 'Hello, World!'
+
+=begin
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+=end
+
+# puts 'Hello, World!'
+# puts 'Hello, World!'
+# puts 'Hello, World!'
+
+# This is a comment.
